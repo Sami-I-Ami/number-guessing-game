940f6a58680a59b499a24502b4d93c588ae8b742
Sami Fink <fink.s.m.8651@gmail.com>
2024-04-17 10:09:16 +0000

    feat: added login for new players

--- number_guess.sh
+++ number_guess.sh
@@ -5,3 +5,20 @@ PSQL="psql --username=freecodecamp --dbname=number_guess --tuples-only -c"
 SECRET_NUMBER=$(( RANDOM % 1000 + 1 ))
 CURRENT_GUESS_AMT=0
 
+# Login
+# read username
+echo Enter your username:
+read USERNAME
+
+# get player's info from database
+PLAYER_INFO=$($PSQL "SELECT games_played, best_game FROM guess_info WHERE username = '$USERNAME'")
+
+# if there is no info
+if [[ -z $PLAYER_INFO ]]
+then
+  # message
+  echo "Welcome, $USERNAME! It looks like this is your first time here."
+
+  # enter info into database
+  INSERT_USER_RESULT=$($PSQL "INSERT INTO guess_info(username) VALUES('$USERNAME')")
+fi
