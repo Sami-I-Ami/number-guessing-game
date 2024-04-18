387b6c3de1d0c898bd869b877bbdd2e89dacc0e8
Sami Fink <fink.s.m.8651@gmail.com>
2024-04-18 10:14:48 +0000

    chore: updated sql

--- number_guess.sql
+++ number_guess.sql
@@ -60,6 +60,19 @@ ALTER TABLE public.guess_info OWNER TO freecodecamp;
 -- Data for Name: guess_info; Type: TABLE DATA; Schema: public; Owner: freecodecamp
 --
 
+INSERT INTO public.guess_info VALUES ('user_1713433930080', 2, 211);
+INSERT INTO public.guess_info VALUES ('user_1713433930081', 5, 157);
+INSERT INTO public.guess_info VALUES ('user_1713434442547', 2, 146);
+INSERT INTO public.guess_info VALUES ('user_1713434442548', 5, 143);
+INSERT INTO public.guess_info VALUES ('user_1713434499958', 2, 135);
+INSERT INTO public.guess_info VALUES ('user_1713434499959', 5, 328);
+INSERT INTO public.guess_info VALUES ('user_1713434861433', 2, 987);
+INSERT INTO public.guess_info VALUES ('user_1713434861434', 5, 42);
+INSERT INTO public.guess_info VALUES ('Sami', 4, 0);
+INSERT INTO public.guess_info VALUES ('user_1713435128574', 2, 376);
+INSERT INTO public.guess_info VALUES ('user_1713435128575', 5, 113);
+INSERT INTO public.guess_info VALUES ('user_1713435180162', 2, 348);
+INSERT INTO public.guess_info VALUES ('user_1713435180163', 5, 104);
 
 
 --
